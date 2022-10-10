8e7ddea70d7ac7c60d698e4031160d2760b04ff5
Jordan Lucier <jordan@jordanlucier.com>
2022-10-10 17:57:30 -0400

    Initial - need to sort out deal with ph sensor + add liquid temp sensor

---
+++ garden.ino
@@ -0,0 +1,131 @@
+#include <ESP8266WiFi.h>
+#include <ESP8266WebServer.h>
+
+#include "private.h"
+#include "sensors.h"
+
+#define PROM_NAMESPACE "garden"
+#define HTTP_METRICS_ENDPOINT "/metrics"
+#define HTTP_SERVER_PORT 80
+#define SENSE_EVERY 10000
+
+ESP8266WebServer http_server(HTTP_SERVER_PORT);
+
+void setup_server() {
+  setup_wifi();
+  setup_http_server();
+}
+
+void setup() {
+  Serial.begin(115200);
+  setup_sensors();
+  setup_server();
+}
+
+void loop() {
+   http_server.handleClient();
+}
+
+// endpoints
+
+void handle_http_root() {
+  static size_t const BUFSIZE = 256;
+  static char const *response_template =
+    "Prometheus ESP8266 Sensor Exporter.\n"
+    "\n"
+    "Referenced heavily from: https://github.com/HON95/prometheus-esp8266-dht-exporter\n"
+    "\n"
+    "Usage: %s\n";
+  char response[BUFSIZE];
+  snprintf(response, BUFSIZE, response_template, HTTP_METRICS_ENDPOINT);
+  http_server.send(200, "text/plain; charset=utf-8", response);
+}
+
+void handle_http_not_found() {
+    http_server.send(404, "text/plain; charset=utf-8", "Not found.");
+}
+
+void handle_http_metrics() {
+  static bool has_sensed = false;
+  static sensor_reading_t reading;
+  static size_t const BUFSIZE = 2048;
+  static char const *response_template =
+      "# HELP " PROM_NAMESPACE "_air_humidity_percent Air humidity.\n"
+      "# TYPE " PROM_NAMESPACE "_air_humidity_percent gauge\n"
+      "# UNIT " PROM_NAMESPACE "_air_humidity_percent %%\n"
+      PROM_NAMESPACE "_air_humidity_percent %f\n"
+      "# HELP " PROM_NAMESPACE "_air_temperature_celsius Air temperature.\n"
+      "# TYPE " PROM_NAMESPACE "_air_temperature_celsius gauge\n"
+      "# UNIT " PROM_NAMESPACE "_air_temperature_celsius \u00B0C\n"
+      PROM_NAMESPACE "_air_temperature_celsius %f\n"
+      "# HELP " PROM_NAMESPACE "_bucket_humidity_percent Bucket humidity.\n"
+      "# TYPE " PROM_NAMESPACE "_bucket_humidity_percent gauge\n"
+      "# UNIT " PROM_NAMESPACE "_bucket_humidity_percent %%\n"
+      PROM_NAMESPACE "_bucket_humidity_percent %f\n"
+      "# HELP " PROM_NAMESPACE "_bucket_temperature_celsius Bucket temperature.\n"
+      "# TYPE " PROM_NAMESPACE "_bucket_temperature_celsius gauge\n"
+      "# UNIT " PROM_NAMESPACE "_bucket_temperature_celsius \u00B0C\n"
+      PROM_NAMESPACE "_bucket_temperature_celsius %f\n"
+      "# HELP " PROM_NAMESPACE "_solution_ph Solution ph.\n"
+      "# TYPE " PROM_NAMESPACE "_solution_ph gauge\n"
+      "# UNIT " PROM_NAMESPACE "_solution_pn pH\n"
+      PROM_NAMESPACE "_solution_pn %f\n"
+      "# HELP " PROM_NAMESPACE "_solution_temperature_celsius Solution temperature.\n"
+      "# TYPE " PROM_NAMESPACE "_solution_temperature_celsius gauge\n"
+      "# UNIT " PROM_NAMESPACE "_solution_temperature_celsius \u00B0C\n"
+      PROM_NAMESPACE "_solution_temperature_celsius %f\n";
+
+  if (!has_sensed || millis() - reading.millis > 10000) {
+    reading = read_sensors();
+    if (!reading.dht1.success || !reading.dht2.success) {
+        http_server.send(500, "text/plain; charset=utf-8", "Sensor error.");
+        return;
+    }
+
+    has_sensed = true;
+  }
+
+  char response[BUFSIZE];
+  snprintf(
+    response,
+    BUFSIZE,
+    response_template,
+    reading.dht1.humidity,
+    reading.dht1.temp,
+    reading.dht2.humidity,
+    reading.dht2.temp,
+    reading.ph,
+    reading.liquid_temp
+  );
+  http_server.send(200, "text/plain; charset=utf-8", response);
+}
+
+// Setup / run
+
+void setup_http_server() {
+    Serial.println("Setting up HTTP server");
+    http_server.on("/", HTTPMethod::HTTP_GET, handle_http_root);
+    http_server.on(HTTP_METRICS_ENDPOINT, HTTPMethod::HTTP_GET, handle_http_metrics);
+    http_server.onNotFound(handle_http_not_found);
+    http_server.begin();
+    Serial.println("HTTP server started");
+}
+
+void setup_wifi() {
+  // // Begin WiFi
+  WiFi.begin(WIFI_SSID, WIFI_PASS);
+ 
+  // Connecting to WiFi...
+  Serial.print("Connecting to ");
+  Serial.print(WIFI_SSID);
+  // Loop continuously while WiFi is not connected
+  while (WiFi.status() != WL_CONNECTED) {
+    delay(100);
+    Serial.print(".");
+  }
+ 
+  // Connected to WiFi
+  Serial.println();
+  Serial.print("Connected! IP address: ");
+  Serial.println(WiFi.localIP());
+}
---
+++ sensors.h
@@ -0,0 +1,101 @@
+// REQUIRES the following Arduino libraries:
+// - DHT Sensor Library: https://github.com/adafruit/DHT-sensor-library
+// - Adafruit Unified Sensor Lib: https://github.com/adafruit/Adafruit_Sensor
+
+#include <Adafruit_Sensor.h>
+#include <DHT.h>
+#include <DHT_U.h>
+
+#define DHT1_PIN 13
+#define DHT2_PIN 12
+#define PH_PIN A0
+#define SENSOR_SLOPE -0.00563
+
+DHT_Unified dht1(DHT1_PIN, DHT22);
+DHT_Unified dht2(DHT2_PIN, DHT22);
+
+struct dht_reading_t {
+  float temp; // c
+  float humidity;
+  bool success;
+};
+
+struct sensor_reading_t {
+  dht_reading_t dht1;
+  dht_reading_t dht2;
+  float ph;
+  float liquid_temp;
+  uint millis;
+};
+
+// PH
+
+void setup_ph() {
+}
+
+float read_ph(bool print) {
+
+  delay(2000);
+  float mv = analogRead(PH_PIN) / 1024.0 * 3300;  // read milivolts
+  float ph = SENSOR_SLOPE * (mv - 1500) + 7;
+
+  if (print) {
+    Serial.print("Analog V: ");
+    Serial.println(mv);
+    Serial.print("PH: ");
+    Serial.println(ph, 3);
+  }
+
+  return ph;
+}
+
+
+// DHTs
+
+void setup_dht() {
+  dht1.begin();
+  dht2.begin();
+  sensor_t sensor;
+  dht1.temperature().getSensor(&sensor);
+}
+
+dht_reading_t read_dht(DHT_Unified* dht) {
+  // Get temperature event and print its value.
+  sensors_event_t event;
+  dht_reading_t result;
+  result.success = true;
+
+  dht->temperature().getEvent(&event);
+  result.temp = event.temperature;
+  result.success &= !isnan(event.temperature);
+  dht->humidity().getEvent(&event);
+  result.humidity = event.relative_humidity;
+  result.success &= !isnan(event.relative_humidity);
+  return result;
+}
+
+void print_reading(dht_reading_t* reading) {
+  Serial.print("Temp: ");
+  Serial.print(reading->temp);
+  Serial.println(" F");
+  Serial.print("Humidity: ");
+  Serial.print(reading->humidity);
+  Serial.println("%");
+}
+
+// setup
+
+void setup_sensors() {
+  setup_ph();
+  setup_dht();
+}
+
+sensor_reading_t read_sensors() {
+  return sensor_reading_t {
+    read_dht(&dht1),
+    read_dht(&dht2),
+    read_ph(false),
+    20,
+    millis(),
+  };
+}
